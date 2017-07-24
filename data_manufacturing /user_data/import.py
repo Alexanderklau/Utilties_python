79bf9474676465a4e54a7b2827e39f754c520ffe
lwb <123@admin.com>
2017-07-24 15:19:24 +0800

    Sql_test

--- data_manufacturing /user_data/import.py
+++ data_manufacturing /user_data/import.py
@@ -18,46 +18,51 @@ repayment_id = random.randint(30000,40000)
 bank_number = bank_card.generate_bankcard_num()
 phone_number = Id_card.createPhone()
 card_number = Id_card.gennerator()
+print bank_number
+print phone_number
+print repayment_id
+print chinesename.run()
+print card_number
 name = chinesename.run()
 data = {
     "actual_collection_data":[
         {
             "card_info":{
                 "bank_type":"xxxx",
-                "card_number":bank_number,
+                "card_number":"",
                 "card_type":"3",
                 "phone_no":phone_number
             },
-            "id_card":{
+            "id_ca  rd":{
                 "back_pic":"",
                 "front_pic":"",
                 "handle_pic":""
             },
             "repayment_info":{
-                "accounts_receivable":100,
-                "actual_amount":1000,
-                "amount":1100,
+                "accounts_receivable":"0",
+                "actual_amount":"1000",
+                "amount":"1100",
                 "apply_time":"2016-05-02 17:46:05",
-                "installment_count":1,
-                "installment_day":14,
-                "installment_id":0,
+                "installment_count":"1",
+                "installment_day":"14",
+                "installment_id":"0",
                 "installment_info":[
                     {
-                        "exact_pay_amount":0,
-                        "installment_id":1,
+                        "exact_pay_amount":"0",
+                        "installment_id":"1",
                         "installment_number":1,
-                        "installment_status":2,
-                        "overdue_amount":7340,
-                        "overdue_days":367,
-                        "should_pay_amount":1100,
+                        "installment_status":"2",
+                        "overdue_amount":"7340",
+                        "overdue_days":"367",
+                        "should_pay_amount":"1100",
                         "should_repay_time":"2016-05-16 00:00:00"
                     }
                 ],
                 "pay_time":"2016-05-02 17:46:05",
-                "penalty":20,
-                "repay_amount":1100,
+                "penalty":"20",
+                "repay_amount":"1100",
                 "repayment_id":repayment_id,
-                "rest_amount":1100
+                "rest_amount":"1100"
             },
             "user_info":{
                 "channel":"wechat",
@@ -77,7 +82,7 @@ data = {
                 "gender":"男",
                 "home_address":"河南省周口地区周口地区",
                 "id_no":card_number,
-                "mariage":0,
+                "mariage":"",
                 "name":name,
                 "phone_no":phone_number,
                 "postcode":"级别：A，芝麻分:599，备注：null",
@@ -86,8 +91,9 @@ data = {
                 "work_name":"",
                 "work_number":""
             }
-        }
+        },
     ],
+
     "all_collection_data_length":1,
     "org_account":"wd",
     "org_token":"58ea49418e89ef17dffadac9182ef1556c1a0bde8bd0dcbe163dc1dcb908c529"
